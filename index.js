901020500143cc8266e449da2170d3501b32d264
Tonybrian-code <deluxetony14@gmail.com>
2023-09-26 01:13:20 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,31 @@
+function shout(string) {
+    return string.toUpperCase();
+}
+function whisper(string) {
+    return string.toLowerCase();
+}
+function logShout(string) {
+    console.log("HELLO");
+}
+logShout();
+function logWhisper(string) {
+    console.log("hello");
+}
+logWhisper();
+  function sayHiToHeadphonedRoommate(string) {
+    if (string.toLowerCase() === string) {
+      return "I can\'t hear you!";
+    }
+  }
+  function sayHiToHeadphonedRoommate(string) {
+    if (string.toUpperCase() === string){
+        return "YES INDEED!";
+    }
+  }
+  function  sayHiToHeadphonedRoommate(string) {
+    if (string === "Let's have dinner together!"){
+        return "I would love to!"
+    }
+  }
+  
+
